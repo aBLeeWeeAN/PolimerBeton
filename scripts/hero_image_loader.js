8a9a4c76ddd0d8bb4f650f7c9fcf807aeffee7be
Vladimir Nikulin <mail.jorey@gmail.com>
2024-08-25 01:22:32 +0300

    Add lazy load hero image by dynamic js script

---
+++ scripts/hero_image_loader.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Определяем код для вставки
+    const picture_html = `
+        <picture>
+            <!-- Определяем изображение для самых маленьких экранов -->
+            <source srcset="images/hero/hero_bc_xs.webp" media="(max-width: 320px)" sizes="(max-width: 320px) 100vw">
+            
+            <!-- Определяем изображение для маленьких экранов -->
+            <source srcset="images/hero/hero_bc_sm.webp" media="(max-width: 480px)" sizes="(max-width: 480px) 100vw">
+            
+            <!-- Определяем изображение для средних экранов -->
+            <source srcset="images/hero/hero_bc_md.webp" media="(max-width: 640px)" sizes="(max-width: 640px) 100vw">
+            
+            <!-- Определяем изображение для больших экранов -->
+            <source srcset="images/hero/hero_bc_lg.webp" media="(max-width: 800px)" sizes="(max-width: 800px) 100vw">
+            
+            <!-- Определяем изображение для очень больших экранов -->
+            <source srcset="images/hero/hero_bc_xl.webp" media="(max-width: 1024px)" sizes="(max-width: 1024px) 100vw">
+            
+            <!-- Изображение по умолчанию для случаев, когда медиа-запросы не сработали -->
+            <img src="images/hero/hero_bc_xxl.webp" 
+                 width="1280" 
+                 height="760" 
+                 alt="Задний фон блока Hero"
+                 id="hero-fr-img">
+        </picture>
+    `;
+
+    // Найти контейнер #Hero и вставить код
+    const hero_section = document.getElementById('Hero');
+    if (hero_section) {
+        hero_section.insertAdjacentHTML('beforeend', picture_html);
+    }
+});
