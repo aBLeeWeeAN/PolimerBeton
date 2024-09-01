c16a0846af67f3fdf7873f704402284a577cc83b
Vladimir Nikulin <mail.jorey@gmail.com>
2024-09-01 20:26:58 +0300

    Перевод проекта в режим Release

--- config/settings.py
+++ config/settings.py
@@ -10,13 +10,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-SECRET_KEY = config('DJANGO_SECRET_KEY', default='default-secret-key')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.3.3', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['polimerbeton-vrn.ru', '178.208.81.224']
 
 
 # Application definition
@@ -197,7 +197,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SMTP Yandex
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 587
+# EMAIL_PORT = 587  # basic http
+EMAIL_PORT = 465    # for ssl (https)
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
@@ -206,3 +207,45 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # database encryption key
 FIELD_ENCRYPTION_KEY = config('FIELD_ENCRYPTION_KEY')
+
+
+# Django logging
+LOGGING = {
+    'version': 1,                                   # Версия конфигурации логирования
+    'disable_existing_loggers': False,              # Не отключать существующих логеров
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'ERROR',                       # Минимальный уровень сообщений для записи в файл
+            'class': 'logging.FileHandler',         # Используемый класс для обработки логов
+            'filename': 'errors.log',               # Путь к файлу логов
+            'formatter': 'verbose',                 # Форматирование сообщений
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',       # Вывод в консоль
+            'formatter': 'simple',                  # Форматирование сообщений
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],                   # Используемые обработчики для этого логера
+            'level': 'ERROR',                       # Минимальный уровень сообщений для этого логера
+            'propagate': True,                      # Сообщения должны передаваться дальше по цепочке логеров
+        },
+    },
+    'main_app': {                                   # Логер для вашего приложения
+        'handlers': ['console', 'file'],            # Используемые обработчики
+        'level': 'DEBUG',                           # Минимальный уровень сообщений
+        'propagate': False,                         # Не передавать сообщения дальше
+    },
+}
