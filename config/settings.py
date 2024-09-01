ed05752dbacf4ea5c5af7a9ce3f7b531a3c45666
JoelWillLiveForever <mail.jorey@gmail.com>
2024-09-01 21:33:20 +0000

    Обновлён settings.py. На других файлах были изменены права доступа.

--- config/settings.py
+++ config/settings.py
@@ -198,9 +198,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SMTP Yandex
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
-# EMAIL_PORT = 587  # basic http
-EMAIL_PORT = 465    # for ssl (https)
-EMAIL_USE_TLS = True
+
+
+# EMAIL_PORT = 587      # basic http
+EMAIL_USE_TLS = False
+
+EMAIL_PORT = 465        # for ssl (https)
+EMAIL_USE_SSL = True
+
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
